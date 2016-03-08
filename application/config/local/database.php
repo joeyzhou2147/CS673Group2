1245dc9f28c13a75165c8da4521fa49585d07220
joeyzhou2147 <joeyzhou2147@gmail.com>
2016-03-08 11:45:49 -0500

    'configure_about_the_database'

--- application/config/local/database.php
+++ application/config/local/database.php
@@ -48,10 +48,13 @@
 $active_group = 'default';
 $active_record = TRUE;
 
+// change the local database connection setting here
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['port'] = '3306';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'bp';
+// setting of basic information is done
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
